bef1c3e8fc12a0a5e4feda96929bb5bb6590ab4b
Vladislav <Vladislav4869@gmail.com>
2020-12-28 12:11:26 +0300

    rename

--- JavaLab/src/main/java/ru/itis/service/PDFProducer.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.service;
-
-import com.itextpdf.text.DocumentException;
-
-import java.io.IOException;
-
-public interface PDFProducer {
-    void produce(String title, String body, String code);
-}
